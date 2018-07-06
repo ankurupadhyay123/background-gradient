dba58098a8dbf0b069a6cfd53f248c58ad9f0035
ankurupadhyay123 <ankurupadhyay123@hotmail.com>
2018-07-06 21:54:15 +0530

    added random color generator

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.getElementById("randomButton");
 
 function setGradient() {
 	body.style.background = 
@@ -14,6 +15,26 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+function displayGradientProperty() {
+	console.log("sfdfdfd");
+	css.textContent = body.style.background + ";";	
+}
+
+function generateRandomColor() {
+	color1Value = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	color2Value = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+
+	color1.value = color1Value;
+	color2.value = color2Value;
+	setGradient();
+}
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", generateRandomColor);
+
+window.onload = function() {
+	setGradient();
+}
